59ed6b7c24babb460767401f69c43d2448f3a052
nishananaaah <nishanaf731@gmail.com>
2024-11-11 16:01:06 +0530

    product-user-model

---
+++ Models/productModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+
+const productSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+  },
+  quantity:{
+    type:Number,
+    default:1,
+  },
+  isDeleted:{
+    type:Boolean,
+    default:false,
+  },
+});
+
+const product =  mongoose.model('products',productSchema);
+export default product;
---
+++ Models/userModel.js
@@ -0,0 +1,40 @@
+import mongoose, { Types } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    accountCreateDate:{
+        type:Date,
+        default:Date.now,
+        required:true,
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false,
+    },
+    cart:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Cart"
+    }],
+    wishlist:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Wishlist",
+    }],
+    orders:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Orders"
+    }]
+},{timestamps:true})//Adds createdAt and updatedAt fields automatically
+
+const User = mongoose.model("User",userSchema);
+export default User;
